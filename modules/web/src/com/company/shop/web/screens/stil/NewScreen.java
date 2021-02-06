4c38a5757185368db19ae63f54939bf78f096285
aslomDev <aslomphp@gmail.com>
2021-02-06 16:52:14 +0500

    100% upload file server tg false

---
+++ modules/web/src/com/company/shop/web/screens/stil/NewScreen.java
@@ -0,0 +1,10 @@
+package com.company.shop.web.screens.stil;
+
+import com.haulmont.cuba.gui.screen.Screen;
+import com.haulmont.cuba.gui.screen.UiController;
+import com.haulmont.cuba.gui.screen.UiDescriptor;
+
+@UiController("shop_")
+@UiDescriptor("new-screen.xml")
+public class NewScreen extends Screen {
+}
